8aec3b45a6615ffde59d0fae9f6469df0ec49db2
RickyL-2000 <ruiqili4@illinois.edu>
2022-03-18 00:06:04 +0800

    将颜色曲线加入图像; 解决了x轴对齐的问题

--- music_analyzer/main.py
+++ music_analyzer/main.py
@@ -22,14 +22,38 @@ def plot_beats(y, fs, begin=0, end=10, hop_length=512):
 
     # plot
     plt.figure(figsize=(9, 6))
-    fig, ax = plt.subplots(nrows=2, sharex=True, figsize=(9, 6))
+    fig, ax = plt.subplots(nrows=3, figsize=(12, 12))
     M = librosa.feature.melspectrogram(y=y_, sr=fs, hop_length=hop_length)
-    librosa.display.specshow(librosa.power_to_db(M, ref=np.max), y_axis='mel', x_axis='time', hop_length=hop_length,
-                             ax=ax[0])
+    M = librosa.power_to_db(M, ref=np.max)
+    librosa.display.specshow(M, y_axis='mel', x_axis='time', hop_length=hop_length, ax=ax[0])
+    ax[0].set_xlim(begin, end)
+
     # waveform
     t = np.arange(begin, end, 1 / fs)
-    ax[1].plot(t, y_)
-    ax[1].vlines(times[beats], 0, 1, color='r', linestyles='--')
+    ax[1].plot(t, y_, label='waveform')
+    ax[1].vlines(times[beats], 0, 1, color='r', linestyles='--', label='beat')
+
+    # energy
+    energy = librosa.feature.rms(y_, frame_length=2048, hop_length=hop_length)[0]
+    times = librosa.times_like(energy, sr=fs, hop_length=hop_length) + begin
+    ax[1].plot(times, energy, color='y', label='energy')
+    ax[1].set_xlim(begin, end)
+
+    ax[1].legend()
+
+    # color map
+    color_map = np.zeros_like(energy)
+    beat_sequence = np.zeros_like(energy)
+    beat_sequence[beats] = 1.0
+    beat_gap = 60 / tempo * fs / hop_length
+    energy /= np.max(energy)
+    beat_soft = np.convolve(beat_sequence, np.hamming(2 * beat_gap * 0.4), mode='same')
+    color_map += energy + 0.3 * beat_soft
+    color_map /= np.max(color_map)
+    color_map = np.convolve(color_map, np.ones(8) / 8, mode='same')  # filter
+    color_matrix = np.tile(color_map, (int(color_map.shape[0] / 4), 1))
+    ax[2].imshow(color_matrix, cmap='hsv')
+
     plt.show()
 
 # %%
@@ -106,4 +130,52 @@ plt.show()
 beat_sequence_ = np.convolve(beat_sequence, np.hamming(2 * beat_gap * 0.4), mode='same')
 plt.vlines(times[beats], 0, 1, color='r', linestyles='--', label='beat')
 plt.plot(times, beat_sequence_)
+plt.show()
+
+# %%
+# try plot together
+begin = 0
+end = 10
+hop_length = 512
+
+y_ = y[int(begin * fs): int(end * fs)]
+
+onset_env = librosa.onset.onset_strength(y_, fs, aggregate=np.median)
+tempo, beats = librosa.beat.beat_track(onset_envelope=onset_env, sr=fs, hop_length=hop_length)
+times = librosa.times_like(onset_env, sr=fs, hop_length=hop_length) + begin
+
+# plot
+plt.figure(figsize=(9, 6))
+fig, ax = plt.subplots(nrows=3, figsize=(12, 12))
+M = librosa.feature.melspectrogram(y=y_, sr=fs, hop_length=hop_length)
+M = librosa.power_to_db(M, ref=np.max)
+librosa.display.specshow(M, y_axis='mel', x_axis='time', hop_length=hop_length, ax=ax[0])
+ax[0].set_xlim(begin, end)
+
+# waveform
+t = np.arange(begin, end, 1 / fs)
+ax[1].plot(t, y_, label='waveform')
+ax[1].vlines(times[beats], 0, 1, color='r', linestyles='--', label='beat')
+
+# energy
+energy = librosa.feature.rms(y_, frame_length=2048, hop_length=hop_length)[0]
+times = librosa.times_like(energy, sr=fs, hop_length=hop_length) + begin
+ax[1].plot(times, energy, color='y', label='energy')
+ax[1].set_xlim(begin, end)
+
+plt.legend()
+
+# color map
+color_map = np.zeros_like(energy)
+beat_sequence = np.zeros_like(energy)
+beat_sequence[beats] = 1.0
+beat_gap = 60 / tempo * fs / hop_length
+energy /= np.max(energy)
+beat_soft = np.convolve(beat_sequence, np.hamming(2 * beat_gap * 0.4), mode='same')
+color_map += energy + 0.3 * beat_soft
+color_map /= np.max(color_map)
+color_map = np.convolve(color_map, np.ones(8) / 8, mode='same')  # filter
+color_matrix = np.tile(color_map, (int(color_map.shape[0] / 4), 1))
+ax[2].imshow(color_matrix, aspect='auto', cmap='hsv')
+
 plt.show()
