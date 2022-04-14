198cfdb94172971cc98c0c6fe587be0f0731488b
idlesilver <Haozhe.18@intl.zju.edu.cn>
2022-04-14 20:25:10 +0800

    reconstruct communication system for micropython

--- esp32/main.py
+++ esp32/main.py
@@ -3,8 +3,10 @@ from utils.wifi import blink_connect
 import utils.log as log
 # from utils.uart import uart_loopback
 from utils.channel import Channel
-from utils.config import ESP_IP, PC_IP, BOARDCAST_IP, PORT
+from utils.config import PC_IP, DYNAMICS_PORT, LIGHT_PORT
 import network
+import time
+import random
 
 print("start")
 
@@ -14,17 +16,20 @@ def server_test():
     
     local_host = network.WLAN(network.STA_IF).ifconfig()[0]  # 获取IP地址
     c = Channel("esp_server")
-    c.becomeServer(local_host,PORT,recv)
+    c.becomeServer(local_host,LIGHT_PORT,recv)
     c.listen()
 
 def client_test():
     c = Channel("esp_client")
-    c.becomeClient(PC_IP,PORT)
-    for i in range(10):
-        c.send("msg"+str(i))
+    c.becomeClient(PC_IP,DYNAMICS_PORT)
+    while True:
+        time.sleep(random.random())
+        c.send((random.random()*360,
+                        random.random()*360,
+                        random.random()*360))
 
 
 blink_connect()
 sleep(1)
-server_test()
-# client_test()
+# server_test()
+client_test()
