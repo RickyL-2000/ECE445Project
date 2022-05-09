0c54d4fa6d634f03de442da66f8af1905940b6e2
RickyL-2000 <ruiqili4@illinois.edu>
2022-05-09 19:02:11 +0800

    270 servo control done (360 servo under construction)

---
+++ espidf/components/servo/include/servo.hpp
@@ -0,0 +1,40 @@
+#ifndef _SERVO_HPP
+#define _SERVO_HPP
+
+#include "esp_log.h"
+#include "esp_check.h"
+#include "driver/mcpwm.h"
+
+// You can get these value from the datasheet of servo you use, in general pulse width varies between 1000 to 2000 mocrosecond
+#define SERVO_MIN_PULSEWIDTH_US (500) // Minimum pulse width in microsecond
+#define SERVO_MAX_PULSEWIDTH_US (2500) // Maximum pulse width in microsecond
+#define SERVO_MAX_DEGREE_DS3230 (180)   // Maximum angle in degree of DS3230 (360 / 2 = 180)
+#define SERVO_MAX_DEGREE_DS3218 (135)   // Maximum angle in degree of DS3218 (270 / 2 = 135)
+
+#define DS3230 3230
+#define DS3218 3218
+
+
+class Servo {
+
+private:
+    static constexpr const char *TAG = "servo";
+    int servo_name;    // DS3230, or DS3218
+    int servo_max_degree;
+    gpio_num_t servo_pulse_gpio;
+    mcpwm_unit_t servo_mcpwm_unit;
+    mcpwm_io_signals_t servo_mcpwm_io_signal;
+    mcpwm_timer_t servo_mcpwm_timer;
+    mcpwm_generator_t servo_mcpwm_generator;
+
+    uint32_t angle2dutyus(int angle) const;
+
+public:
+    Servo(int servo_type, gpio_num_t gpio_num,
+          mcpwm_unit_t mcpwm_unit, mcpwm_io_signals_t mcpwm_io_signal,
+          mcpwm_timer_t mcpwm_timer, mcpwm_generator_t mcpwm_generator);
+
+    esp_err_t set_angle(int angle);
+};
+
+#endif //_SERVO_HPP
---
+++ espidf/components/servo/servo.cpp
@@ -0,0 +1,40 @@
+#include "servo.hpp"
+
+Servo::Servo(int servo_type, gpio_num_t gpio_num,
+             mcpwm_unit_t mcpwm_unit, mcpwm_io_signals_t mcpwm_io_signal,
+             mcpwm_timer_t mcpwm_timer, mcpwm_generator_t mcpwm_generator) {
+    if (servo_type == DS3230) {
+        servo_max_degree = SERVO_MAX_DEGREE_DS3230;
+    } else if (servo_type == DS3218) {
+        servo_max_degree = SERVO_MAX_DEGREE_DS3218;
+    }
+    servo_name = servo_type;
+    servo_pulse_gpio = gpio_num;
+    servo_mcpwm_unit = mcpwm_unit;
+    servo_mcpwm_io_signal = mcpwm_io_signal;
+    servo_mcpwm_timer = mcpwm_timer;
+    servo_mcpwm_generator = mcpwm_generator;
+
+    ESP_ERROR_CHECK(mcpwm_gpio_init(mcpwm_unit, mcpwm_io_signal, servo_pulse_gpio));
+
+    mcpwm_config_t pwm_config = {
+            .frequency = 50, // frequency = 50Hz, i.e. for every servo motor time period should be 20ms
+            .cmpr_a = 0,     // duty cycle of PWMxA = 0
+            .cmpr_b = 0,
+            .duty_mode = MCPWM_DUTY_MODE_0,
+            .counter_mode = MCPWM_UP_COUNTER,
+    };
+    ESP_ERROR_CHECK(mcpwm_init(mcpwm_unit, mcpwm_timer, &pwm_config));
+}
+
+uint32_t Servo::angle2dutyus(int angle) const {
+    return (angle + servo_max_degree) * (SERVO_MAX_PULSEWIDTH_US - SERVO_MIN_PULSEWIDTH_US) /
+            (2 * servo_max_degree) + SERVO_MIN_PULSEWIDTH_US;
+}
+
+esp_err_t Servo::set_angle(int angle) {
+    ESP_ERROR_CHECK(
+            mcpwm_set_duty_in_us(servo_mcpwm_unit, servo_mcpwm_timer, servo_mcpwm_generator, angle2dutyus(angle))
+            );
+    return ESP_OK;
+}
