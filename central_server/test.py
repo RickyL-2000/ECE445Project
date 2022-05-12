dda9908c1086d4650adab76e925633329ad56303
RickyL-2000 <ruiqili4@illinois.edu>
2022-05-12 13:38:44 +0800

    music analyzer: unupload test

--- central_server/test.py
+++
@@ -1,9 +0,0 @@
-# %%
-from music_analyzer import MusicAnalyzer
-
-if __name__ == "__main__":
-    manalyzer = MusicAnalyzer()
-    f_path = "music/qilixiang.mp3"
-    manalyzer.gen_color_seq(f_path)
-
-
